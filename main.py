7824ae996ee1b46c5c3da6ab37a679517069cf7d
bgulley25 <brycegulley@gmail.com>
2025-01-31 14:43:09 -0500

    2025_01-31_GulleyBryce_Assignment_2-4

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # input statements
-salary = float(input())
-numDependents = float(input())
+salary = 60000
+numDependents = 3
 
 # calculate taxes here
-
+stateTax = (salary * .065)
+federalTax = (salary * .28)
+dependentDeduction = (salary * .075)
+totalWitholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWitholding
 # output statements
+print("State Tax: $" + str(stateTax))
+print("Federal Tax: $" + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
